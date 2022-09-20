4587db7500f1f2da04f075ecbbf9d843831bb81b
sculleywr1 <sculleywr1@gmail.com>
2022-09-20 12:07:41 -0500

    Added documentation for the Regex Pin Validation package and removed unnecessary print line statement.

--- src/regexPinValidation/MainDriver.java
+++ src/regexPinValidation/MainDriver.java
@@ -7,6 +7,7 @@ public class MainDriver {
 
 		String[] pins = {"121317", "1234", "45135", "89abc1", "900876", " 4983"};
 		
+		// Cycles through the pins and says which pins are valid.
 		for (int i = 0; i < pins.length; i++) 
 		{
 			boolean isValid = Validation.validate(pins[i]);
--- src/regexPinValidation/Validation.java
+++ src/regexPinValidation/Validation.java
@@ -4,17 +4,17 @@ public class Validation {
 	
 	public static boolean validate(String input)
 	{
-		System.out.println(input);
+		// Returns true if the operation was successful false otherwise.
 		boolean result = false;
+		// Generates a regular expression pattern for the first acceptable character pattern.
 		String pattern1 = "^\\d{6}$";
+		// Returns true if the input matches the pattern defined by the regular expression in pattern1.
 		boolean pattern1Match = input.matches(pattern1);
-		System.out.println(pattern1Match);
 		
 		String pattern2 = "^\\d{4}$";
 		boolean pattern2Match = input.matches(pattern2);
 		
-		System.out.println(pattern2Match);
-		
+		// Checks if at least one pattern matches. If they do, change the boolean to true
 		if (pattern1Match || pattern2Match)
 		{
 			result = true;
